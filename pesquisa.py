aada710a1ec8d2aad15d9240e7b6643d41f44731
James <james.peres.s@gmail.com>
2016-09-26 02:32:00 -0300

    pesquisa com historico

--- pesquisa.py
+++ pesquisa.py
@@ -6,7 +6,7 @@ arq = input(str("digite o nome do arquivo: "))
 palavra_in = input(str("Digite a palavra a ser contada: "))
 
 
-# retirar a acentuacao // codigo de github.com/etandel //
+# retirar a acentuacao
 def remove_diacritic(s):
 	return (normalize('NFKD', s)
 			.encode('ascii', 'ignore')
@@ -22,3 +22,16 @@ content = f.read()
 palavra = (remove_diacritic(content)).lower().split()
 
 print(palavra.count(palavra_in))
+
+
+
+# Grava um historico das pesquisas feitas
+arquivo = open('teste_de_hist.txt', 'r')
+conteudo = arquivo.readlines()
+
+conteudo.append('\n\nLivro lido: %s'%arq)
+conteudo.append('\nPalavra procurada: %s'%palavra_in)
+conteudo.append('\nVezes encontrada: %s'%(palavra.count(palavra_in)))
+
+arquivo = open('teste_de_hist.txt', 'w')
+arquivo.writelines(conteudo)
