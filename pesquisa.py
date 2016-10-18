19c5bea02a2bb12f0eb80be497a0b43e4d61d48c
James Peres <james.peres.s@gmail.com>
2016-10-18 14:49:33 -0200

    work

--- pesquisa.py
+++ pesquisa.py
@@ -1,5 +1,4 @@
 # coding: utf-8
-
 from unicodedata import normalize
 
 #colocar data
@@ -11,9 +10,10 @@ print (" \n    Pesquisa de nomes     \n")
 print (30 * '-')
 
 
+
+
 arq = input(str("\ndigite o nome do arquivo: "))
 palavra_in = input(str("Digite a palavra a ser contada: "))
-print()
 
 
 # retirar a acentuacao  // codigo de github.com/etandel //
@@ -26,16 +26,25 @@ arq = arq + ".txt"
 
 f = open(arq, 'r', encoding="iso-8859-1")
 content = f.read()
-palavra = (remove_diacritic(content)).lower().split()
+palavras = remove_diacritic(content).lower()
 
-print(palavra.count(palavra_in))
+palavras.replace(".", "")
+palavras.replace("\"","")
+palavras.replace(",", "")
+palavras.replace("!", "")
+palavras.replace("?", "")
+
+
+palavras.split()
+
+print(palavras.count(palavra_in))
 
 # Grava um historico das pesquisas feitas
 arquivo = open('teste_de_hist.txt', 'a')
 
 arquivo.writelines('\n\nLivro lido: %s'%arq)
 arquivo.writelines('\nPalavra procurada: %s'%palavra_in)
-arquivo.writelines('\nVezes encontrada: %s'%(palavra.count(palavra_in)))
+arquivo.writelines('\nVezes encontrada: %s'%(palavras.count(palavra_in)))
 arquivo.writelines('\nPesquisado em: %s/%s/%s'%(now.day, now.month, now.year))
 
 
