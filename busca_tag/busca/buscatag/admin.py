53f2fe5376b71e33ae1559fe22360e3f22cb69d8
James Peres <james.peres.s@gmail.com>
2017-01-05 20:20:46 -0200

    add

--- busca_tag/busca/buscatag/admin.py
+++ busca_tag/busca/buscatag/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Pessoa, Tag, PessoaTag
 
-# Register your models here.
+class PessoaAdmin(admin.ModelAdmin):
+    pass
+
+
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+class PessoaTagAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Pessoa, PessoaAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(PessoaTag, PessoaTagAdmin)
--- busca_tag/busca/buscatag/models.py
+++ busca_tag/busca/buscatag/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=50)
+    mail = models.EmailField()
+    projeto = models.CharField(max_length=50)
+
+
+class Tag(models.Model):
+    nometag = models.CharField(max_length=50)
+
+class PessoaTag (models.Model):
+    id_pessoa = models.ForeignKey(Pessoa)
+    id_tag = models.ForeignKey(Tag)
+    rating = models.IntegerField()
+    obs = models.CharField(max_length=200)
