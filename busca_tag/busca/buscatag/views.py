4efb139ffc6b6f84587b1d37bd57acecfa8221de
James Peres <james.peres.s@gmail.com>
2017-01-05 21:59:50 -0200

    add

--- busca_tag/busca/buscatag/views.py
+++ busca_tag/busca/buscatag/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.template.loader import render_to_string
 
-# Create your views here.
+
+from .models import PessoaTag, Pessoa, Tag
+
+def tag_lista(request):
+    tags_localizados = Tag.objects.all()
+    return render(request, 'busca/lista.html', {'tags': tags_localizados})
